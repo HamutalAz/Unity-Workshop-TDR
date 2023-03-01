3b792ebacf28917fcc994c0037ae62f17e7cab4b
itaylevy0305 <itay03065@gmail.com>
2023-03-01 12:14:33 +0200

    not working functions!

--- TDR/Assets/Scripts/DataBaseManager.cs
+++ TDR/Assets/Scripts/DataBaseManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Firebase.Database;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class DataBaseManager : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class DataBaseManager : MonoBehaviour
     public TextMeshProUGUI feedbackLBL;
     private string userID;
     private DatabaseReference dbReference;
+    private const int MAXPLAYERS = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +24,62 @@ public class DataBaseManager : MonoBehaviour
 
     public void CreateUser()
     {
-        User newUser = new User(userName.text);
-        string json = JsonUtility.ToJson(newUser);
         Debug.Log("trying to create user:" + userName.text);
-        dbReference.Child("users").Child(userID).SetRawJsonValueAsync(json);
+        try
+        {
+            CheckIfUserNameExists(userName.text);
+            User newUser = new User(userName.text);
+            string json = JsonUtility.ToJson(newUser);
+            string key = dbReference.Child("Lobby").Push().Key;
+            dbReference.Child("Lobby").Child(key).SetRawJsonValueAsync(json);
+            Debug.Log("Successfully creatued user '" + newUser.user_name + "'!");
+            //move to next scene!
+        } catch (Exception e)
+        {
+            feedbackLBL.text = e.Message;
+        }
+        
+        
+        
+       
+        
+        //dbReference.Child("Lobby").SetRawJsonValueAsync(json);
 
+
+
+        //idea: add a list of users. before adding, get the list from datasnapshot, and check if users exists there. If not, add him and add the list back to
+        // /Lobby!
+          
         //todo: later check if such user exists, if so change feedback label to a better feedback.
-        feedbackLBL.text = "trying to create user with username: " + userName.text;
+        //feedbackLBL.text = "trying to create user with username: " + userName.text;
     }
-    
+
+    public void CheckIfUserNameExists(string userName)
+    {
+        dbReference.Child("Lobby").GetValueAsync().ContinueWith(task =>
+        {
+
+            if (task.IsCompleted)
+            {
+                var json = task.Result.GetRawJsonValue();
+
+                var users = task.Result.Value as Dictionary<string, object>;
+                if (users!= null)
+                {
+                    //check if lobby isn't full
+                    if (users.Count > MAXPLAYERS)
+                    {
+                        throw new Exception("Lobby is full! can't join!");
+                    }
+                    //check if user name is unique
+                    foreach (KeyValuePair<string, object> item in users.Values)
+                    {
+                        if (item.Value.Equals(userName)){
+                            throw new Exception("A user with the name of '" + userName + "' already exists!");
+                        }
+                    }
+                } 
+            }
+        });
+    }
 }
--- TDR/Assets/Scripts/User.cs
+++ TDR/Assets/Scripts/User.cs
@@ -1,9 +1,23 @@
+using System;
+using System.Collections.Generic;
 public class User
 {
+    
     public string userName;
 
     public User(string userName)
     {
         this.userName = userName;
     }
+
+
+    public string user_name
+    {
+        get => userName;
+    }
+
+    public override string ToString() {
+        return userName;
+    }
+
 }
