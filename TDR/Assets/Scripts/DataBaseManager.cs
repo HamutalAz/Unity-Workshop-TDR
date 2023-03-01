6606caf2ef2cb730713a2321b8454ea2165e1048
HamutalAz <76840545+HamutalAz@users.noreply.github.com>
2023-03-01 16:52:36 +0200

    some changes for login - mainly check for username exists
    some changes for the login page:
    before logging in a user, we check if the username exists in db.
    after the log-in succeed, lobby screen is uploaded.

--- TDR/Assets/Scripts/DataBaseManager.cs
+++ TDR/Assets/Scripts/DataBaseManager.cs
@@ -5,6 +5,10 @@ using Firebase.Database;
 using UnityEngine.UI;
 using TMPro;
 using System;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
+using UnityEngine.Tilemaps;
+using Firebase.Extensions;
+using UnityEngine.SceneManagement;
 
 public class DataBaseManager : MonoBehaviour
 {
@@ -13,7 +17,6 @@ public class DataBaseManager : MonoBehaviour
     private string userID;
     private DatabaseReference dbReference;
     private const int MAXPLAYERS = 5;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -22,64 +25,80 @@ public class DataBaseManager : MonoBehaviour
 
     }
 
-    public void CreateUser()
+    public async void CreateUser()
     {
         Debug.Log("trying to create user:" + userName.text);
         try
         {
-            CheckIfUserNameExists(userName.text);
+            // check if username 'userName.text' already exists
+            await dbReference.Child("Lobby").OrderByChild("userName").EqualTo(userName.text).GetValueAsync().ContinueWithOnMainThread(task =>
+            {
+                if (task.IsCanceled)
+                {
+                    Debug.Log("FirebaseDatabaseError: IsCanceled: " + task.Exception);
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.Log("FirebaseDatabaseError: IsFaulted:" + task.Exception);
+                    return;
+                }
+
+                DataSnapshot snapshot = task.Result;
+                if (snapshot.ChildrenCount != 0)
+                {
+                    throw new Exception("Username '" + userName.text + "' already exists. please try again.");
+
+                }
+            });
+
+            // create new user
             User newUser = new User(userName.text);
             string json = JsonUtility.ToJson(newUser);
             string key = dbReference.Child("Lobby").Push().Key;
-            dbReference.Child("Lobby").Child(key).SetRawJsonValueAsync(json);
-            Debug.Log("Successfully creatued user '" + newUser.user_name + "'!");
-            //move to next scene!
-        } catch (Exception e)
+            await dbReference.Child("Lobby").Child(key).SetRawJsonValueAsync(json);
+            Debug.Log("Successfully created user '" + newUser.user_name + "'!");
+            feedbackLBL.text = "Successfully created user '" + newUser.user_name + "'";
+
+            // move to next scene!
+            SceneManager.LoadScene("WaitingRoom");
+            return;
+        }
+        catch (Exception e)
         {
             feedbackLBL.text = e.Message;
+            return;
         }
-        
-        
-        
-       
-        
-        //dbReference.Child("Lobby").SetRawJsonValueAsync(json);
-
-
-
-        //idea: add a list of users. before adding, get the list from datasnapshot, and check if users exists there. If not, add him and add the list back to
-        // /Lobby!
-          
-        //todo: later check if such user exists, if so change feedback label to a better feedback.
-        //feedbackLBL.text = "trying to create user with username: " + userName.text;
     }
 
-    public void CheckIfUserNameExists(string userName)
-    {
-        dbReference.Child("Lobby").GetValueAsync().ContinueWith(task =>
-        {
+    // used for previous version: I didn't erase it in case you'll need it (hamutal)
+    //public async void CheckIfUserNameExists(string userName)
+    //{
+    //    dbReference.Child("Lobby").GetValueAsync().ContinueWith(task =>
+    //    {
 
-            if (task.IsCompleted)
-            {
-                var json = task.Result.GetRawJsonValue();
+    //        if (task.IsCompleted)
+    //        {
+    //            var json = task.Result.GetRawJsonValue();
 
-                var users = task.Result.Value as Dictionary<string, object>;
-                if (users!= null)
-                {
-                    //check if lobby isn't full
-                    if (users.Count > MAXPLAYERS)
-                    {
-                        throw new Exception("Lobby is full! can't join!");
-                    }
-                    //check if user name is unique
-                    foreach (KeyValuePair<string, object> item in users.Values)
-                    {
-                        if (item.Value.Equals(userName)){
-                            throw new Exception("A user with the name of '" + userName + "' already exists!");
-                        }
-                    }
-                } 
-            }
-        });
-    }
+    //            var users = task.Result.Value as Dictionary<string, object>;
+    //            if (users != null)
+    //            {
+    //                //check if lobby isn't full
+    //                if (users.Count > MAXPLAYERS)
+    //                {
+    //                    throw new Exception("Lobby is full! can't join!");
+    //                }
+    //                //check if user name is unique
+    //                foreach (KeyValuePair<string, object> item in users.Values)
+    //                {
+    //                    if (item.Value.Equals(userName))
+    //                    {
+    //                        throw new Exception("A user with the name of '" + userName + "' already exists!");
+    //                    }
+    //                }
+    //            }
+    //        }
+    //    });
+    //}
 }
