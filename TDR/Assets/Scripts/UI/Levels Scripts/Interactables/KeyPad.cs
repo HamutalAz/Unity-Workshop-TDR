58a23ec383d59e98ed876e0a42edc05ffdfdf5f4
HamutalAz <76840545+HamutalAz@users.noreply.github.com>
2023-03-27 16:02:17 +0300

    working with door opening - anumations, interactivity

--- TDR/Assets/Scripts/UI/Levels Scripts/Interactables/KeyPad.cs
+++ TDR/Assets/Scripts/UI/Levels Scripts/Interactables/KeyPad.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class KeyPad : Interactable
 {
+    [SerializeField]
+    private GameObject door;
+    private bool isOpen;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +22,7 @@ public class KeyPad : Interactable
 
     protected override void Interact()
     {
-        Debug.Log("Interracted with " + gameObject.name);
+        isOpen = !isOpen;
+        door.GetComponent<Animator>().SetBool("IsOpen", isOpen);
     }
 }
