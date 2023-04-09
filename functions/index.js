47d46bf6f676f49bac1b87e718d75221ca4d5115
HamutalAz <76840545+HamutalAz@users.noreply.github.com>
2023-04-09 20:30:59 +0300

    creating a cloud function to check num of players in lobby - not done.
    listening on changes in DB & checking amount of players in the room.
    also starting to write game creating function - have the base working but not done yet.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,47 @@
 const functions = require("firebase-functions");
-
 const regionalFunctions = functions.region("europe-west1");
-exports.helloWorld = regionalFunctions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const firestore = regionalFunctions.firestore;
+const InGamePlayersAmount = 10;
+const lobbyPath = "Lobby/gYdtPMVaorwoc2jH3Iog/room_members/{user_id}";
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+
+// listen on player's joinning the game
+exports.checkNumberOfPlayersInLobby = firestore
+    .document(lobbyPath).onCreate((snap, context)=>{
+      console.log("***** checkNumberOfPlayersInLobby: invoked! *****");
+      // get all players from the lobby & check how many player in there
+      const lobbyCol = db.collection("Lobby");
+      const lobbyDoc = lobbyCol.doc("gYdtPMVaorwoc2jH3Iog");
+      return lobbyDoc.collection("room_members").get()
+          .then((snapshot) => {
+            let size = 0;
+            console.log("**** getting snapshot data! ****");
+            snapshot.forEach((doc) => {
+              console.log(doc.id, " => ", doc.data());
+              size = size + 1;
+            });
+            console.log("there are " + size + " players in lobby");
+            // if enough players in lobby: create a new game & delete
+            // the players from the lobby
+            if (size >= InGamePlayersAmount) {
+              console.log("size >= " + InGamePlayersAmount +
+              ", creating new game!");
+              // create a new game
+              // delete players from lobby
+            }
+          });
+    });
+
+// create a new game - currently hard codded - work needed!
+exports.createGame = functions.https.onRequest((request, response) => {
+  console.log("creating game!");
+  const newGameRef = db.collection("Games").doc();
+  console.log("new game ref" + newGameRef);
+  const newGameMembersRef = newGameRef.collection("room_members");
+  console.log("newGameMembersRef" + newGameMembersRef);
+  return newGameMembersRef.doc("FKBQ6nK7pRcltvGYiKqB").set({
+    userName: "matityahu",
+  });
 });
-
-
