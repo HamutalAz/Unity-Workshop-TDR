b4617c1121d3303eceb234c3a3936cf6df62308b
HamutalAz <76840545+HamutalAz@users.noreply.github.com>
2023-04-10 12:54:30 +0300

    finish function that check players in loby & transfer to new game

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 const db = admin.firestore();
 
-// listen on player's joinning the game
+// listen on player's joinning the game & move then to game
 exports.checkNumberOfPlayersInLobby = firestore
     .document(lobbyPath).onCreate((snap, context)=>{
       console.log("***** checkNumberOfPlayersInLobby: invoked! *****");
@@ -17,31 +17,56 @@ exports.checkNumberOfPlayersInLobby = firestore
       return lobbyDoc.collection("room_members").get()
           .then((snapshot) => {
             let size = 0;
-            console.log("**** getting snapshot data! ****");
+            const arr = [];
+            // console.log("**** getting snapshot data! ****");
             snapshot.forEach((doc) => {
-              console.log(doc.id, " => ", doc.data());
+              // console.log(doc.id, " => ", doc.data());
+              const dict = {
+                "id": doc.id,
+                "userName": doc.data().userName,
+              };
+              arr.push(dict);
               size = size + 1;
             });
             console.log("there are " + size + " players in lobby");
             // if enough players in lobby: create a new game & delete
             // the players from the lobby
             if (size >= InGamePlayersAmount) {
-              console.log("size >= " + InGamePlayersAmount +
-              ", creating new game!");
-              // create a new game
-              // delete players from lobby
+              /* console.log("size >= " + InGamePlayersAmount +
+              ", creating new game!"); */
+              createNewGame(arr);
+              deleteFromLobby(arr);
             }
           });
     });
 
-// create a new game - currently hard codded - work needed!
-exports.createGame = functions.https.onRequest((request, response) => {
-  console.log("creating game!");
+/** creating a new game collection & adding users
+ * @param {arr} arr - an array of dictionarys containing users data.
+ * @return {promise}
+ */
+function createNewGame(arr) {
+  console.log("***** creating game! *****");
   const newGameRef = db.collection("Games").doc();
-  console.log("new game ref" + newGameRef);
   const newGameMembersRef = newGameRef.collection("room_members");
-  console.log("newGameMembersRef" + newGameMembersRef);
-  return newGameMembersRef.doc("FKBQ6nK7pRcltvGYiKqB").set({
-    userName: "matityahu",
+  return arr.forEach((val) => {
+    newGameMembersRef.doc(val.id).set({
+      userName: val.userName,
+    });
   });
-});
+}
+
+/** deleting users from the lobby
+ * @param {arr} arr - an array of dictionarys containing users data.
+ * @return {promise}
+ */
+function deleteFromLobby(arr) {
+  console.log("***** deleting from lobby! *****");
+  let lobbyCol = db.collection("Lobby");
+  const lobbyDoc = lobbyCol.doc("gYdtPMVaorwoc2jH3Iog");
+  lobbyCol = lobbyDoc.collection("room_members");
+  return arr.forEach((val) => {
+    lobbyCol.doc(val.id).delete().then(() => {
+      // console.log("Document " + val.id + " successfully deleted!");
+    });
+  });
+}
