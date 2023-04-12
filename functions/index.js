aef9cf2aa807a11fe1f6735e2a6727d8bc81bfe3
itaylevy0305 <itay03065@gmail.com>
2023-04-12 20:59:28 +0300

    room allocation algo completed

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const regionalFunctions = functions.region("europe-west1");
 const firestore = regionalFunctions.firestore;
-const InGamePlayersAmount = 5;
+const InGamePlayersAmount = 6;
 const lobbyPath = "Lobby/gYdtPMVaorwoc2jH3Iog/lobby_members/{user_id}";
 const admin = require("firebase-admin");
 admin.initializeApp();
@@ -33,9 +33,10 @@ exports.checkNumberOfPlayersInLobby = firestore
               size = size + 1;
             });
             console.log("there are " + size + " players in lobby");
-              createNewGame(arr);
+              createNewGame(arr,numOfPlayers);
+              //matchMaking(arr,numOfPlayers, null);
               //rooms allocation algorithm
-              deleteFromLobby(arr);
+              //deleteFromLobby(arr);
               
           });
       }
@@ -48,20 +49,33 @@ exports.checkNumberOfPlayersInLobby = firestore
  * @param {arr} arr - an array of dictionarys containing users data.
  * @return {promise}
  */
-function createNewGame(arr) {
+function createNewGame(arr,numOfPlayers) {
   console.log("***** creating game! *****");
   const newGameRef = db.collection("Games").doc();
   const newGameMembersRef = newGameRef.collection("game_members");
-  return arr.forEach(async (val) => {
-    //creates a document for each player
-    newGameMembersRef.doc(val.id).set({
-      userName: val.userName,
+
+  arr.forEach(async (val) => {
+      //creates a document for each player
+      await newGameMembersRef.doc(val.id).set({
+        userName: val.userName,
+      });
+      //updates the gameId under Users/{user-id}
+      await db.collection("Users").doc(val.id).update({
+        gameId: newGameRef.id
+      });
     });
-    //updates the gameId under Users/{user-id}
-     db.collection("Users").doc(val.id).update({
-      gameId: newGameRef.id
-    });
-  });
+
+  return matchMaking(arr,numOfPlayers,newGameRef.id);
+  // return arr.forEach(async (val) => {
+  //   //creates a document for each player
+  //   await newGameMembersRef.doc(val.id).set({
+  //     userName: val.userName,
+  //   });
+  //   //updates the gameId under Users/{user-id}
+  //   await db.collection("Users").doc(val.id).update({
+  //     gameId: newGameRef.id
+  //   });
+  // });
 }
 
 /** deleting users from the lobby
@@ -78,4 +92,94 @@ function deleteFromLobby(arr) {
       // console.log("Document " + val.id + " successfully deleted!");
     });
   });
+}
+
+/** creating rooms for players
+ * @param {arr, numOfPlayers} - an array of dictionarys containing users data, number of users.
+ * @return {promise}
+ */
+
+async function matchMaking(arr, numOfPlayers, gameId){
+  console.log("MatchMaking!!");
+  let isFinal = true;
+  let numberOfPlayersInRoom = 1;
+  if(numOfPlayers > 3){
+      isFinal = false;
+      numberOfPlayersInRoom = setNumberOfPlayersInRoom(numOfPlayers);
+  }
+  const array = getShuffledArray(numOfPlayers);
+
+  for(i=0;i<numOfPlayers/numberOfPlayersInRoom;i++){
+    //creates a document for the room at Rooms/{roomId}.
+    const roomRef = db.collection("Rooms").doc();
+    //creates a document for the room at Games/{gameId}/game_rooms/{roomId}
+
+    // await db.collection("Games").doc(gameId).collection("game_rooms")
+    //  .doc(roomRef.id).set();
+    
+    //updates the gameId under Rooms/{roomId}
+    roomRef.set({
+      gameId: gameId
+    })
+    for(j=0;j<numberOfPlayersInRoom;j++){
+      const user = arr[numberOfPlayersInRoom*i + j];
+      //creates a document for the player at Rooms/{roomId}/room_members/{userId}
+      await roomRef.collection("room_members")
+      .doc(user.id).set({
+        userName: user.userName,
+      });
+      //updates roomId at Users/{userId}
+    }
+
+
+    //create room document in Rooms/{roomId} => done
+    //update room in Games/{gameID} /game_rooms/{roomId} => done
+    //random players to rooms => done
+    //update room in Users/{userId}/roomID => not done!
+    
+    //update game info => numofplayers, num in room, num to eliminate, room name etc.
+    
+  }
+}
+
+function setNumberOfPlayersInRoom(numOfPlayers){
+  //**final**
+  if(numOfPlayers <= 3){
+    return 1;
+  }
+  const choices = [1];
+  if(numOfPlayers % 2 == 0)
+      choices.push(2);
+  if(numOfPlayers % 3 == 0)
+      choices.push(3);
+  return choices[getRandomInt(choices.length)];
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getShuffledArray(numOfPlayers){
+  arr = [];
+  for(i=0;i<numOfPlayers;i++){
+    arr.push(i);
+  }
+  return shuffle(arr);
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
 }
