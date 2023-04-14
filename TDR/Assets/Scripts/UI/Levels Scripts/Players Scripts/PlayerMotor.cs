836e426b5278f96cd72e036bb626499f8acdede4
HamutalAz <76840545+HamutalAz@users.noreply.github.com>
2023-04-14 16:51:20 +0300

    creating game panels and syncing player's move

--- TDR/Assets/Scripts/UI/Levels Scripts/Players Scripts/PlayerMotor.cs
+++ TDR/Assets/Scripts/UI/Levels Scripts/Players Scripts/PlayerMotor.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Firebase.Extensions;
+using Firebase.Firestore;
 using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
@@ -10,11 +12,17 @@ public class PlayerMotor : MonoBehaviour
     public float speed = 5;
     public float gravity = -9.8f;
     public float jumpHeight = 1.5f;
+    private static FirebaseFirestore dbReference;
+    private DocumentReference userDoc;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        dbReference = FirebaseFirestore.DefaultInstance;
+        //userDoc = dbReference.Collection("Users").Document(DataBaseManager.userID);
+        userDoc = dbReference.Collection("Users").Document("JQD1GEkcogVOfGodZ1Y5"); // todo: delete & uncomment above line
+
     }
 
     // Update is called once per frame
@@ -35,6 +43,22 @@ public class PlayerMotor : MonoBehaviour
 
         if (IsGrounded && playerVelocity.y < 0)
             playerVelocity.y = -2f;
+
+        //update location in DB if movment detected
+        if(moveDirection != Vector3.zero) {
+            string loc = transform.position.ToString();
+            Dictionary<string, object> updates = new Dictionary<string, object>
+            {
+                { "Location", loc }
+            };
+
+            userDoc.UpdateAsync(updates).ContinueWithOnMainThread(task =>
+            {
+                Debug.Log(
+                        "Location updated to: " + loc);
+            });
+        }
+
     }
     public void Jump()
     {
