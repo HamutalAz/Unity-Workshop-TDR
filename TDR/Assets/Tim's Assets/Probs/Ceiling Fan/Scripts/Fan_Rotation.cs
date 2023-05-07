30ba2bbf4d85b470a45fc0cf52bb87b112bc4fa7
HamutalAz <76840545+HamutalAz@users.noreply.github.com>
2023-05-07 15:50:01 +0300

    creating level

---
+++ TDR/Assets/Tim's Assets/Probs/Ceiling Fan/Scripts/Fan_Rotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Numerics;
+using UnityEngine;
+
+
+
+public class Fan_Rotation : MonoBehaviour {
+
+	//variable for speed
+	public float speed=100;
+	private int ind = 0;
+    private List<int> directions = new List<int>() { 0, -4, 0, -4, 0, 4 , 0, 4, 0, -4, 0, 4, 0, 0};
+
+    private void Start()
+    {
+        InvokeRepeating("changeDirection", 0, 0.3f);
+
+    }
+
+    void Update () {
+        // rotate fan blades 
+        transform.Rotate((new Vector3(0, 0, directions[ind])) * Time.deltaTime * speed);
+    }
+
+    private void changeDirection()
+    {
+        if (ind < directions.Count - 1)
+            ind++;
+        else
+            ind = 0;
+    }
+}
