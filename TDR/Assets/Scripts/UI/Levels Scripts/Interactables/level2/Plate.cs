f434980a19e9e28e551f2c22fd0a0f7fb3c5cd70
HamutalAz <76840545+HamutalAz@users.noreply.github.com>
2023-05-10 19:54:53 +0300

    making plate (in level2) interactable

---
+++ TDR/Assets/Scripts/UI/Levels Scripts/Interactables/level2/Plate.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+
+public class Plate : Interactable
+{
+    [SerializeField]
+    private bool isReadable = false;
+    private string owner = null;
+    public LevelHandler levelHandler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelHandler.addLevelObject("plate", this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void Interact()
+    {
+
+        Debug.Log("**** interact with plate!!!");
+
+        // create dictionary with the data we want to send to the DB
+        Dictionary<string, object> data = new Dictionary<string, object>
+        {
+                { "owner", DataBaseManager.userID },
+                { "key", "owner" }
+            };
+
+        // todo: send request to DB to put the plate in backpack
+        //DataBaseManager.instance.levelManager.LaunchRequest("putInBackPack", "plate", data);
+    }
+
+    // Apply received changes
+    public override void UpdateUI(Dictionary<string, object> data)
+    {
+        Debug.Log("updating data from the DB!");
+
+        owner = (string)data["owner"];
+
+        // put in backPack or set visable false if belongs to someone else.
+        if(owner != null)
+        {
+            if (owner == DataBaseManager.userID)
+            {
+                Debug.Log("you own the plate!");
+                // put in backPack
+            }
+            else
+            {
+                Debug.Log("someone else owns the plate!");
+                // make it unactive (unviable)
+                gameObject.SetActive(!gameObject.activeSelf);
+            }
+        }
+
+        Debug.Log("owner:" + owner);
+    }
+}
