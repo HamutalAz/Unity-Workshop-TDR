df41446407f6564ff6b726630bc82afc9ea824b4
HamutalAz <76840545+HamutalAz@users.noreply.github.com>
2023-05-27 19:03:07 +0300

    undo double click

--- TDR/Assets/Scripts/UI/Levels Scripts/BackPackManager.cs
+++ TDR/Assets/Scripts/UI/Levels Scripts/BackPackManager.cs
@@ -21,9 +21,9 @@ public class BackPackManager : MonoBehaviour
     LevelHandler levelHandler;
 
     // backPack double click data
-    float clicked = 0;
-    float clicktime = 0;
-    float clickdelay = 0.5f;
+    //float clicked = 0;
+    //float clicktime = 0;
+    //float clickdelay = 0.5f;
 
     bool dropInLoc = false;
     Vector3 location = Vector3.zero;
@@ -90,20 +90,23 @@ public class BackPackManager : MonoBehaviour
             EventTrigger.Entry entry = new EventTrigger.Entry();
             entry.eventID = EventTriggerType.PointerClick;
             entry.callback.AddListener((eventData) => {
+                // Double click
 
-                clicked++;
+                //clicked++;
 
-                if (clicked == 1)
-                    clicktime = Time.time;
+                //if (clicked == 1)
+                //    clicktime = Time.time;
 
-                if (clicked > 1 && Time.time - clicktime < clickdelay)
-                {
-                    clicked = 0;
-                    clicktime = 0;
-                    dropOutOfBackPack(img);
-                }
-                else if (clicked > 2 || Time.time - clicktime > 1)
-                    clicked = 0;
+                //if (clicked > 1 && Time.time - clicktime < clickdelay)
+                //{
+                //    clicked = 0;
+                //    clicktime = 0;
+                //    dropOutOfBackPack(img);
+                //}
+                //else if (clicked > 2 || Time.time - clicktime > 1)
+                //    clicked = 0;
+                dropOutOfBackPack(img);
+
             });
 
             trigger.triggers.Add(entry);
