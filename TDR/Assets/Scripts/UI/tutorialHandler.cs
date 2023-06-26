9171eb9dccc056c7f5a6d487aae947841d207903
HamutalAz <76840545+HamutalAz@users.noreply.github.com>
2023-06-26 15:41:34 +0300

    creating second tutorial panel

---
+++ TDR/Assets/Scripts/UI/tutorialHandler.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialHandler : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> panelsArr = new();
+    int currInd = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void nextClick()
+    {
+        Debug.Log("count:" + panelsArr.Count);
+        if(currInd < panelsArr.Count-1)
+        {
+            Debug.Log("currInd:" + currInd);
+            currInd++;
+            if (currInd >= 1)
+                panelsArr[currInd - 1].SetActive(false);
+            panelsArr[currInd].SetActive(true);
+        }
+        else
+        {
+            Debug.Log("no more!");
+        }
+    }
+
+    public void backClick()
+    {
+        if (currInd > 0)
+        {
+            currInd--;
+            if (currInd + 1 < panelsArr.Count)
+                panelsArr[currInd + 1].SetActive(false);
+            panelsArr[currInd].SetActive(true);
+        }
+    }
+}
